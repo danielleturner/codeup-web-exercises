af0f18fe0ab57cfb77873079c3bafa40b57d6933
Danielle Turner <danielle.7.turner@gmail.com>
2021-11-22 13:37:23 -0600

    Hello

--- strings.js
+++ strings.js
@@ -1,10 +1,10 @@
 var myName = "Danny";
-var aSentence = "hi " + myName + "how " + "are " + "you " + "doing ";
+var aSentence = "hi " + myName + " how " + "are " + "you " + "doing? ";
 console.log(aSentence);
 
 aSentence += " and I am your benevolent"
 console.log(aSentence);
 
-aSentence += " \nyou shall \nlisten to me \nalways \nhahaha";
+aSentence += " \nyou shall \nlisten to me \nalways\n \nhahaha";
 console.log(aSentence);
 
