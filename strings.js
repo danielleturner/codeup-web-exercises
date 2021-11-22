54c1ea032fe6f5ea2697f66a807fa971b0a4eb94
Danielle Turner <danielle.7.turner@gmail.com>
2021-11-22 13:06:16 -0600

    strings.js

---
+++ strings.js
@@ -0,0 +1,10 @@
+var myName = "Danny";
+var aSentence = "hi " + myName + "how " + "are " + "you " + "doing ";
+console.log(aSentence);
+
+aSentence += " and I am your benevolent"
+console.log(aSentence);
+
+aSentence += " \nyou shall \nlisten to me \nalways \nhahaha";
+console.log(aSentence);
+
