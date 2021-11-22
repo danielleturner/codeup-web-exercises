c75809579ae349f13e6d233aaf9b9e1d8745a309
Danielle Turner <danielle.7.turner@gmail.com>
2021-11-22 16:31:24 -0600

    Nan

---
+++ workingwithtypes.js
@@ -0,0 +1,118 @@
+/*var a = 1;
+console.log(a);
+
+var b = a++;
+console.log(b);
+
+var c = ++a;
+console.log(c);
+
+var d = "hello";
+d++;
+console.log(d);
+
+var e = false;
+e++;
+console.log(e);
+
+var perplexed;
+perplexed + 2;
+console.log(perplexed);
+
+var price = 2.7;
+price.toFixed(2);
+console.log(2);
+
+var price = "2.7";
+price.toFixed(2);
+console.log(2);
+*/
+
+
+console.log(isNaN(0));
+
+console.log(isNaN(1));
+
+console.log(isNaN(""));
+
+console.log(isNaN("string"));
+
+console.log(isNaN("0"));
+
+console.log(isNaN("1"));
+
+console.log(isNaN("3.145"));
+
+console.log(isNaN("3.145"));
+
+console.log(isNaN(Number.MAX_VALUE));
+
+console.log(isNaN(Infinity));
+
+console.log(isNaN("true"));
+
+console.log(isNaN(true));
+
+console.log(isNaN("false"));
+
+console.log(isNaN(false));
+
+/*console.log(Nan == Nan);*/
+
+console.log(!true);
+
+console.log(!false);
+
+console.log(!!true);
+
+console.log(!!false);
+
+console.log(!!0);
+
+console.log(!!-0);
+
+console.log(!!1);
+
+console.log(!!-1);
+
+console.log(!!0.1);
+
+var sample = "Hello, Codeup";
+sample.toLowerCase();
+//console.log(sample.toLowerCase());
+
+sample += " Students";
+//console.log(sample);
+
+sample = sample.replace("Students", "Class");
+// console.log(sample);
+
+// console.log(sample.substr(cIndex, length()))
+
+
+
+var sample = "hello, Codeup";
+console.log(sample.length);
+
+[Answer from above]
+false
+false
+false
+true
+false
+false
+false
+false
+false
+false
+true
+false
+true
+false
+false
+true
+true
+false
+false
+false
+true
