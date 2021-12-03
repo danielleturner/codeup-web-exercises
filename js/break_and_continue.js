a3e0a7e05c14fc755307feb5723c84a0d3cc1d06
Danielle Turner <danielle.7.turner@gmail.com>
2021-12-03 09:50:32 -0600

    Loop Exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -23,3 +23,28 @@ let numberInput = prompt('Please pick an odd number between 1 and 50')
      }
  }
 
+var userNumber = 0;
+do{
+    userNumber = Number(prompt('Please enter and odd number'))
+    if (userNumber < 1 || userNumber > 50) {
+        alert('Number out of bounds')
+    }else if ( userNumber%2 === 0) {
+        alert('Number is not odd')
+    }else if(isNaN(userNumber)) {
+        alert('That is not a number')
+    }else {
+        alert('Yaay your number is good')
+        break;
+    }
+
+}while(true)
+
+console.log('Number to skip: ' + userNumber)
+
+for (var i = 1; i <= 50; i+=2) {
+    if(i === userNumber){
+        console.log('Yikes: skipping number: ' + userNumber)
+        continue;
+    }
+}console.log('Here is an odd: ' +i)
+
