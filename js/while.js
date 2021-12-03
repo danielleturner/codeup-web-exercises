abe072ed1dfa23bb2b3a463cdca26307038f832e
Danielle Turner <danielle.7.turner@gmail.com>
2021-12-03 10:01:17 -0600

    Do while Exercises

--- js/while.js
+++ js/while.js
@@ -7,16 +7,24 @@
 
 // var i = 2;
 //
-// while (i < 65536) {
-//     console.log(parseFloat(65536).toFixed2)); {
-//         console.log(i)
-//     }
+// while (i <= 65536) {
+//     console.log(i);
+//         i *= 2
 // }
 
+var allCones = Math.floor(Math.random() * 50) + 50;
 
-var c = 2;
+do {
 
-while ( c < 65536) {
-    console.log(c);
-    c
-}
+    var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+
+    if(conesToPurchase <= allCones) {
+        console.log(conesToPurchase + "cones sold...")
+        allCones -= conesToPurchase
+    }else {
+        console.log('Can not sell you ' + conesToPurchase + 'I only have' + allCones + '....')
+    }
+console.log(allCones)
+}while (allCones > 0)
+console.log('Yaay I sold all cones')
+
