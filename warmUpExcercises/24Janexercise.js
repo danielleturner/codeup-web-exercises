4c7c77101edfac444927a0233225a0e88df5609b
Danielle Turner <danielle.7.turner@gmail.com>
2022-01-24 09:24:02 -0600

    morning-warmup

---
+++ warmUpExcercises/24Janexercise.js
@@ -0,0 +1,12 @@
+//PROBLEM:  write a function that accepts a string of numbers and returns the sum of those numbers
+//INPUT 1: "42958372" --- EXPECTED OUTPUT 1: 40
+//INPUT 2: "917485"      --- EXPECTED OUTPUT 2: 34
+
+
+let x = "";
+let y = "";
+
+ function sumNum(x, y) {
+    return x + y;
+}
+console.log(sumNum(42958372, 917485));
